023e4120918614fedf95d0ec31ada13c5eaa455d
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-07-24 23:00:52 +0500

    use of Props

---
+++ props/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Cricketer from './Student';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<Cricketer name="Faf Du Plessis" roll="005" />,document.getElementById('root'));
