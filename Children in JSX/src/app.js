967135ac240707f34dbd8759017da4b9eae63bcb
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-07-25 22:22:22 +0500

    Children in JSX

---
+++ Children in JSX/src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+// Functional Component
+const Anything = props =>{
+return <h1> Hello Sarfraz {props.children}</h1>
+}
+export default Anything;
+// same we can use props.children in class component
---
+++ Children in JSX/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Anything from './app.js';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<Anything>I'm Learning React</Anything>,document.getElementById('root'));
