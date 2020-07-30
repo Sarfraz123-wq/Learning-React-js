0b8176d325833e36941f7b7cdf7213c17e25ba4f
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-07-30 15:34:41 +0500

    Introduction to Hooks

---
+++ Introduction + Concepts/src/index.js
@@ -0,0 +1,15 @@
+// Introduction to Hooks
+// Hooks are functions that let you "hook into" React state and lifecycle features from function componnets.
+// Hooks allows you to use React without classes. It means you can use state and other React features without writing a class.
+// React provides a few built in hooks like useState, useEffect etc
+// Hooks are a new addition in React 16.8
+
+// When use React
+//-----------------
+// If you write a function componnet and realize you need to add some state to it.
+// Rules of Hooks
+//----------------
+// Only call Hooks at the top level -- We shouldn't call hooks inside loops,conditions,or nested functions.
+// Only call Hooks from react functions or from custom Hooks. We should not call Hooks from regular JS functions.
+// Hooks don't work in classes. 
+ 
