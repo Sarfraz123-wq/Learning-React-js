0f9b1927a4c4a30305b4115e6a0c78feef4e3038
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-08-02 17:18:17 +0500

    Use of  IIFE function

---
+++ IIFE/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Guest from './Guest'
+import User from './User'
+
+export default class App extends Component {
+    state = {
+        isLogged : false
+    }
+    loginFunction = ()=>{
+       // return <User />
+         this.setState({isLogged : true});
+    }
+    logoutFunction = ()=>{
+        this.setState({isLogged : false});
+        //return <Guest />
+    }
+    render() {
+        const kyaLoginHe = this.state.isLogged;
+        return(
+            <div>
+                {
+                    ( //this is opening brace of IIFI
+                        ()=>{
+                            if(kyaLoginHe){
+                                return <User logoutData={this.logoutFunction}/>
+                            }
+                            else{
+                                return <Guest loginData={this.loginFunction}/>
+                            }
+                        }
+                    )()
+                    //last () is self execution of IIFI.
+                }
+            </div>
+
+           )
+    }
+}
